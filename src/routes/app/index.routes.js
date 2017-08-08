5b1536fc3e20e31d76b3996421ef8f94a426642d
Vlado Tešanović <vlado.tesanovic@gmail.com>
2017-08-08 17:06:53 +0200

    added ui

--- src/routes/app/index.routes.js
+++ src/routes/app/index.routes.js
@@ -1,9 +1,9 @@
 const router = require('express').Router()
 
+const config = require('../../config/app.config')
+
 router.get('/', (req, res) => {
-
-
-  return res.render('index', { title: 'Jahorina ski hike' })
+  return res.render('index', { title: 'Jahorina ski hike', city: config.city, apiKey: config.key })
 })
 
 module.exports = router
