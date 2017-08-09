66ed04b046b16c1d6934f02e381a684b5559717c
Milan Brčkalo <brckalo_milan5@outlook.com>
2017-08-09 11:52:24 +0200

    fixed view, added app routes

--- src/config/app.config.js
+++ src/config/app.config.js
@@ -1,14 +1,9 @@
 module.exports = {
   key: 'AIzaSyB_SkJda2qonoJg10ttnfmunOwDgZweEcc',
-  city: [
-    {
-      name: 'Pale',
-      location: {
-        'lat': 43.8118946,
-        'lng': 18.5711013
-      },
-      radius: '3000',
-      categories: require('google-place-types')
-    }
-  ]
+  city: {
+    name: 'Pale',
+    location: '43.8118946,18.5711013',
+    radius: '3000',
+    categories: require('google-place-types')
+  }
 }
--- src/routes/app/index.routes.js
+++ src/routes/app/index.routes.js
@@ -1,9 +1,27 @@
 const router = require('express').Router()
 
+const config = require('../../config/app.config')
+const queryBuilder = require('../../services/query-builder.service')
+const googleService = require('../../services/google.service')
+
 router.get('/', (req, res) => {
+  return res.render('index', { title: 'Jahorina ski hike', city: config.city })
+})
 
+router.get('/search', (req, res) => {
+  const path = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?`
 
-  return res.render('index', { title: 'Jahorina ski hike' })
+  const url = queryBuilder.create(path, req.query, config.key)
+
+  googleService.getPlaces(url)
+    .then((data) => {
+
+      return res.render('index', { title: 'Jahorina ski hike', city: config.city, place: data })
+    })
+    .catch((error) => {
+      return res.status(400).json(error)
+    })
+
 })
 
 module.exports = router
